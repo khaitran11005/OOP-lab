1d645f63fd8ae6c55def00e1a7b36aed082397ac
khaitran11005 <thdk235509@gmail.com>
2024-11-19 13:05:42 +0700

    Applying Release Flow
    Pushing all lab projects directly into master.

---
+++ OtherProjects/225.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class DoubleCalc {
+    public static void main(String[] args) {
+        Scanner numscan = new Scanner(System.in);
+        System.out.println("First number:");
+        Double num1 = numscan.nextDouble();
+        System.out.println("Second number:");
+        Double num2 = numscan.nextDouble();
+        if (num2 == 0){
+            System.out.println("Division by 0 is not well-defined. Please try a different number.");
+        }
+        else {
+            Double sum = num1 + num2;
+            Double diff = num1 - num2;
+            Double prod = num1 * num2;
+            Double quot = num1 / num2;
+            System.out.println("Sum: " + sum + ", Difference: " + diff +", Product: " + prod + ", Quotient: " + quot);
+        }
+
+    }
+}
---
+++ OtherProjects/66/src/MatrixAddition.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class MatrixAddition{
+	
+	static int[][] MatrixAdder(int[][] mat1, int[][] mat2) {
+		int row = mat1.length;
+		int col = mat1[0].length;
+		if (row != mat2.length || col != mat2[0].length) {
+			throw new NumberFormatException("Matrices must be of the same size to perform addition.");
+		}
+		
+		int[][] result = new int[row][col];
+		for (int i = 0; i < result.length; i++) {
+			for (int k = 0; k < result[i].length; k++) {
+				result[i][k] = mat1[i][k] + mat2[i][k];
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Insert number of rows: ");
+		int rows = scanner.nextInt();
+		System.out.println("Insert number of columns: ");
+		int columns = scanner.nextInt();
+		int[][] mat1 = new int[rows][columns];
+		int[][] mat2 = new int[rows][columns];
+		
+		System.out.println("Insert values for the first matrix: ");
+		for (int i = 0; i < rows; i++) {
+			for (int k = 0; k < columns; k++) {
+				System.out.println("Value for matrix 1, row " + (i+1) + ", column " + (k+1) + ":");
+				mat1[i][k] = scanner.nextInt();
+			}
+		}
+		
+		System.out.println("Insert values for the second matrix: ");
+		for (int i = 0; i < rows; i++) {
+			for (int k = 0; k < columns; k++) {
+				System.out.println("Value for matrix 2, row " + (i+1) + ", column " + (k+1) + ":");
+				mat2[i][k] = scanner.nextInt();
+			}
+		}
+		int[][] output = MatrixAdder(mat1, mat2);
+		System.out.println("Addition result");
+		for (int i = 0; i < rows; i++) {
+			for (int k = 0; k < columns; k++) {
+				System.out.print(output[i][k] + " ");
+			}
+			System.out.println("");
+		}
+		scanner.close();
+	}
+}
