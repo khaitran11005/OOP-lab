0c99d02397beb2e253d6d63d7a2ffaa5e5cef759
khaitran11005 <thdk235509@gmail.com>
2024-11-26 14:17:34 +0700

    [Lab04] Section 6 - Creating the Playable interface
    Created Playable interface

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,13 +1,12 @@
 package hust.soict.dsai.aims.media;
 
-public class DigitalVideoDisc extends Disc {
+public class DigitalVideoDisc extends Disc implements Playable {
 	
 	private static int nbDigitalVideoDiscs = 0;
 
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
-		this.id = nbDigitalVideoDiscs;
 		nbDigitalVideoDiscs += 1;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
@@ -15,16 +14,13 @@ public class DigitalVideoDisc extends Disc {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
-		this.id = nbDigitalVideoDiscs;
 		nbDigitalVideoDiscs += 1;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
-		this.director = director;
 		this.cost = cost;
-		this.id = nbDigitalVideoDiscs;
 		nbDigitalVideoDiscs += 1;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -34,7 +30,6 @@ public class DigitalVideoDisc extends Disc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
-		this.id = nbDigitalVideoDiscs;
 		nbDigitalVideoDiscs += 1;
 	}
 	
@@ -52,4 +47,10 @@ public class DigitalVideoDisc extends Disc {
 			return false;
 		}
 	}
+	
+	public void play() {
+		System.out.println("Playing DVD: " + this.getTitle());
+		System.out.println("DVD length: " + this.getLength());
+	}
+	
 }
